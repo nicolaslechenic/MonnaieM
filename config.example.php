4151c635aeae71a2d3079989555b6f3427d8040e
Nicolas Le Chenic <nicolaslechenic@gmail.com>
2017-08-15 17:31:43 +0200

    clean: move configuration example to config.example.php

---
+++ config.example.php
@@ -0,0 +1,16 @@
+<?php
+  // Access to the database 
+  $host="";
+  $user="";
+  $pwd="";
+  $db="";
+
+  // Mailing address used by MySQL errors messages
+  $admin="";
+
+  // From mailing address used by the application
+  $from="";
+
+  // Monnaie M base url
+  $baseurl="";
+?>
